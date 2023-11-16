1479fd5d440ca67285f5f26bdecce3abaf8a55df
Manuel Cano <isc.juan.manuel.cr@gmail.com>
2023-11-16 03:08:45 -0600

    Generacion de proyecto base

---
+++ src/test/java/com/jmcr/consumomarvelapirest/ConsumomarvelapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmcr.consumomarvelapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumomarvelapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
